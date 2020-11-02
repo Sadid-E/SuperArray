fcd30db415923a3fb42cea1118b06700b5a7bdd8
Sadid-E <sethun20@stuy.edu>
2020-11-02 18:03:15 -0500

    Added variables, constuctor, and size()

---
+++ SuperArray.java
@@ -0,0 +1,20 @@
+public class SuperArray {
+
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String[10];
+  }
+
+  public int size() {
+    int size = 0;
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] != null) {
+        size++;
+      }
+    }
+    return size;
+  }
+
+}
