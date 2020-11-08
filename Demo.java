d2a652f4eda590f6506185ef40cd4349c8950b5b
Sadid-E <sethun20@stuy.edu>
2020-11-07 20:50:44 -0500

    Added the findOverlap method

--- Demo.java
+++ Demo.java
@@ -8,6 +8,20 @@ public class Demo {
     }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray overlap = new SuperArray();
+    removeDuplicates(a);
+    removeDuplicates(b);
+    for (int i = 0; i < a.size(); i++) {
+      for (int j = 0; j < a.size(); j++) {
+        if (a.get(i).equals(b.get(j))) {
+          overlap.add(a.get(i));
+        }
+      }
+    }
+    return overlap;
+  }
+
   public static void main(String[]args) {
     SuperArray words = new SuperArray();
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
@@ -16,6 +30,12 @@ public class Demo {
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+
+    SuperArray a = new SuperArray();
+    SuperArray b = new SuperArray();
+    a.add("9"); a.add("1"); a.add("2"); a.add("2"); a.add("3"); a.add("4");
+    b.add("0"); b.add("4"); b.add("2"); b.add("2"); b.add("9");
+    System.out.println(findOverlap(a,b));
   }
 
 }
